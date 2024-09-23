97e367daed94b8a01f24b99ccd3c6bad5446a9b9
SatishJoshi01 <satishjoshi792@gmail.com>
2024-09-23 15:51:45 +0530

    Changes in main Method

--- ImagePractice/src/main/java/com/sparken/ImagePractice/ImagePracticeApplication.java
+++ ImagePractice/src/main/java/com/sparken/ImagePractice/ImagePracticeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ImagePracticeApplication {
 
 	public static void main(String[] args) {
+		System.out.println("I m in main Method");
 		SpringApplication.run(ImagePracticeApplication.class, args);
 		
 	}
